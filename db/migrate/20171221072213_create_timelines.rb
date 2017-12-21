ac559bd705e69ba17767d7a1029bfb254d9aa641
flmil <reiserver629412@gmail.com>
2017-12-21 17:03:51 +0900

    first commit

---
+++ db/migrate/20171221072213_create_timelines.rb
@@ -0,0 +1,12 @@
+class CreateTimelines < ActiveRecord::Migration[5.1]
+	def change
+		create_table :timelinetables do |t|
+			t.string :username
+			t.string :tweettime
+			t.string :tweettext
+			t.string :favocount
+			t.string :retwcount
+			t.timestamps null: false
+		end
+	end
+end
